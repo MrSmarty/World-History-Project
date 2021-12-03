e3ada64acced78188a825a828c5e5174bde200e3
Lincoln Smith <lincoln@Stadias-MacBook-Pro.local>
2021-12-03 09:12:57 -0600

    Initial Commit

---
+++ showhide.js
@@ -0,0 +1,12 @@
+function showHide(id) {
+    var x = document.getElementById(id);
+    if (x.style.display == "none") {
+        document.getElementById("achaea").style.display = "none";
+        document.getElementById("britannia").style.display = "none";
+        document.getElementById("judea").style.display = "none";
+        document.getElementById("gaul").style.display = "none";
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
