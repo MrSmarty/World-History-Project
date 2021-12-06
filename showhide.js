0a12fb1da77c6c1379fea5c9d702d9a40a754928
Lincoln Smith <MrSmartyMcFarty@gmail.com>
2021-12-06 07:15:32 -0600

    Commit

--- showhide.js
+++ showhide.js
@@ -5,6 +5,7 @@ function showHide(id) {
         document.getElementById("britannia").style.display = "none";
         document.getElementById("judea").style.display = "none";
         document.getElementById("gaul").style.display = "none";
+        document.getElementById("rome").style.display = "none";
         x.style.display = "block";
     } else {
         x.style.display = "none";
